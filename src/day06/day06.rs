1c142adb445cc0bffff112e5e85525773fe6eef8
Kodlak15 <stanlcod15@gmail.com>
2023-12-06 19:34:43 -0800

    Work on day06

---
+++ src/day06/day06.rs
@@ -0,0 +1,66 @@
+// -------------------------------------------------------
+// Advent of Code 2023 - Day 6
+// -------------------------------------------------------
+
+use crate::read_input;
+
+// -------------------------------------------------------
+// Custom data structures
+// -------------------------------------------------------
+
+// -------------------------------------------------------
+// Main program logic
+// -------------------------------------------------------
+
+// Let x = the number of whole ms the button is held down
+// let n = the number of whole ms in the race
+// speed(x) = x mm/ms
+// time(n, x) = n - x ms
+// distance(n, x) = speed(x) * time(n, x) mm = x * (n - x) mm = x*n - x**2 mm
+//
+// d/dx distance(n, x) = n - 2*x
+// Global maxima at x = n/2
+
+fn pt1(input: &str) -> u32 {
+    0
+}
+
+#[allow(dead_code)]
+fn pt2(input: &str) -> u32 {
+    0
+}
+
+pub fn day06() {
+    let input = read_input("./src/day06/puzzle_input.txt");
+    println!("Day 6:");
+    println!("Part 1: {}", pt1(&input));
+    println!("Part 2: {}", pt2(&input));
+    println!("-------------------------------------------------------")
+}
+
+// -------------------------------------------------------
+// Tests
+// -------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_pt1() {
+        let puzzle_input = "\
+\
+";
+
+        // assert_eq!(pt1(puzzle_input), 35);
+    }
+
+    #[test]
+    fn test_pt2() {
+        let puzzle_input = "\
+\
+";
+
+        // assert_eq!(pt2(puzzle_input), 46);
+    }
+}
