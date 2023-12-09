5d945b1c7c66d6068ab2ab4a2f9005c052f80543
Kodlak15 <stanlcod15@gmail.com>
2023-12-09 15:38:30 -0800

    Work on day09

--- src/day09/day09.rs
+++ src/day09/day09.rs
@@ -22,13 +22,7 @@ fn pt1(input: &str) -> i32 {
         .map(|line| {
             let mut history: Vec<Vec<i32>> = vec![line
                 .split(" ")
-                .map(|c| {
-                    {
-                        println!("c = {:?}", c);
-                        c.parse::<i32>()
-                    }
-                    .expect("Unable to parse character!")
-                })
+                .map(|c| { c.parse::<i32>() }.expect("Unable to parse character!"))
                 .collect()];
 
             while history[history.len() - 1].iter().any(|x| *x != 0) {
@@ -61,13 +55,50 @@ fn pt1(input: &str) -> i32 {
         .sum()
 }
 
-fn pt2(input: &str) -> usize {
-    0
+fn pt2(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut history: Vec<Vec<i32>> = vec![line
+                .split(" ")
+                .map(|c| { c.parse::<i32>() }.expect("Unable to parse character!"))
+                .collect()];
+
+            while history[history.len() - 1].iter().any(|x| *x != 0) {
+                let last = &history[history.len() - 1];
+
+                let next: Vec<i32> = last
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(i, _)| match i < last.len() - 1 {
+                        true => Some(last[i + 1] - last[i]),
+                        false => None,
+                    })
+                    .collect();
+
+                history.push(next);
+            }
+
+            // New logic goes here
+
+            let mut values: Vec<i32> = Vec::new();
+            values.push(0);
+
+            for (i, _) in history.iter().enumerate().rev().skip(1) {
+                let seq = &history[i];
+                let m = seq.len();
+
+                values.push(seq[m - 1] + values[values.len() - 1])
+            }
+
+            values[values.len() - 1]
+        })
+        .sum()
 }
 
 pub fn day09() {
     let input = read_input("./src/day09/puzzle_input.txt");
-    println!("Day 8:");
+    println!("Day 9:");
     println!("Part 1: {}", pt1(&input));
     println!("Part 2: {}", pt2(&input));
     println!("-------------------------------------------------------")
