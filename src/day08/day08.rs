06deeaae8189e75252b6efe083e8894a1f2356bf
Kodlak15 <stanlcod15@gmail.com>
2023-12-08 17:30:35 -0800

    Complete day08

---
+++ src/day08/day08.rs
@@ -0,0 +1,151 @@
+// -------------------------------------------------------
+// Advent of Code 2023 - Day 8
+// -------------------------------------------------------
+
+use std::collections::HashMap;
+
+use crate::read_input;
+
+// -------------------------------------------------------
+// Custom Data Structures
+// -------------------------------------------------------
+
+struct Map {
+    instructions: String,
+    nodes: HashMap<String, (String, String)>,
+}
+
+impl Map {
+    fn from(input: &str) -> Self {
+        let instructions = input
+            .lines()
+            .nth(0)
+            .expect("Could not find the instructions!")
+            .to_string();
+
+        let nodes: HashMap<String, (String, String)> = input
+            .lines()
+            .skip(2)
+            .map(|line| {
+                let splitln: Vec<&str> = line.split(" = ").collect();
+                let name = splitln[0].to_string();
+                let pair: Vec<&str> = splitln[1].split(", ").collect();
+                let left = pair[0].to_string().replace("(", "");
+                let right = pair[1].to_string().replace(")", "");
+
+                (name, (left, right))
+            })
+            .collect();
+
+        Self {
+            instructions,
+            nodes,
+        }
+    }
+}
+
+// -------------------------------------------------------
+// Helper Functions
+// -------------------------------------------------------
+
+fn next_instruction(instructions: &String, n: usize) -> char {
+    instructions
+        .chars()
+        .nth(n % instructions.len())
+        .expect("Failed to get next instruction!")
+}
+
+// -------------------------------------------------------
+// Main Program Logic
+// -------------------------------------------------------
+
+fn pt1(input: &str) -> usize {
+    let map = Map::from(input);
+
+    let mut n = 0;
+    let mut instruction = next_instruction(&map.instructions, n);
+    let mut node = "AAA".to_string();
+    let end_node = "ZZZ".to_string();
+
+    while node != end_node {
+        node = match instruction {
+            'L' => map.nodes[&node].0.clone(),
+            'R' => map.nodes[&node].1.clone(),
+            _ => panic!("Invalid instruction!"),
+        };
+
+        n += 1;
+        instruction = next_instruction(&map.instructions, n);
+    }
+
+    n
+}
+
+fn pt2(input: &str) -> usize {
+    0
+}
+
+pub fn day08() {
+    let input = read_input("./src/day08/puzzle_input.txt");
+    println!("Day 8:");
+    println!("Part 1: {}", pt1(&input));
+    println!("Part 2: {}", pt2(&input));
+    println!("-------------------------------------------------------")
+}
+
+// -------------------------------------------------------
+// Tests
+// -------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1_pt1() {
+        let puzzle_input = "\
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)\
+";
+
+        assert_eq!(pt1(puzzle_input), 2);
+    }
+
+    #[test]
+    fn test2_pt1() {
+        let puzzle_input = "\
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)\
+";
+
+        assert_eq!(pt1(puzzle_input), 6);
+    }
+
+    #[test]
+    fn test_pt2() {
+        let puzzle_input = "\
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)\
+    ";
+
+        assert_eq!(pt2(puzzle_input), 6);
+    }
+}
---
+++ src/day08/notes.md
@@ -0,0 +1,23 @@
+# Rules
+- Check hand type first
+```
+five of a kind > four of a kind > full house > three of a kind > two pair > one pair > high card
+```
+- If the hand type is the same, the hand with the more valuable card at the leftmost index where the cards do not equal one another wins
+```
+A > K > Q > J > T > 9 > 8 > 7 > 6 > 5 > 4 > 3 > 2
+
+Example:
+77878 > 77788
+```
+- Rank the cards
+```
+# If there are 5 cards:
+Greatest value card gets rank 5 ... Lowest value card gets rank 1
+```
+- Winnings are the sum of the products of each hands rank with the associated bid for that hand
+
+# Notes
+- In Rust, you can implement PartialOrd on custom data structure (such as enums and structs) to get ordering behavior
+- Additionally, if you have one such data structure nested within another, Rust is able to use the ordering of the interior data structure to break any ties that occur in the parent 
+- In this particular case, I was able to sort my hands in ascending order, and the cards within were automatically handled when doing so
