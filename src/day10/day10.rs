c8d8d6a3e9b57dbd813c09117ed454c7cd018895
Kodlak15 <stanlcod15@gmail.com>
2023-12-10 03:00:14 -0800

    Work on day10

--- src/day10/day10.rs
+++ src/day10/day10.rs
@@ -1,8 +1,8 @@
 // -------------------------------------------------------
-// Advent of Code 2023 - Day 8
+// Advent of Code 2023 - Day 10
 // -------------------------------------------------------
 
-use std::rc::Rc;
+use std::collections::HashMap;
 
 use crate::read_input;
 
@@ -10,7 +10,20 @@ use crate::read_input;
 // Custom Data Structures
 // -------------------------------------------------------
 
+// Consider using a recursive function to traverse the diagram and find a solution
+// - Start at starting pipe ('S')
+// - Let rec be the recursive function that traverses the diagram and finds the result
+// - Let i, j represent the cell in which the starting pipe resides
+// - rec(i, j) = max(rec(i - 1, j), rec(i + 1, j), rec(i, j - 1), rec(i, j + 1))
+// - If a nil value is encountered ('.') then the current path is not part of the loop
+// - When the starting pipe is found again the loop has been closed, return the number of steps
+// taken
+// - I am inclined to say the result should be the total number of steps in the loop divided by 2,
+// which would be halfway around the loop
+
+#[derive(Debug)]
 enum Pipe {
+    Start,
     Horizontal,
     Vertical,
     NorthEast,
@@ -23,6 +36,7 @@ enum Pipe {
 impl Pipe {
     fn from(c: char) -> Self {
         match c {
+            'S' => Pipe::Start,
             '|' => Pipe::Vertical,
             '-' => Pipe::Horizontal,
             'L' => Pipe::NorthEast,
@@ -35,24 +49,72 @@ impl Pipe {
     }
 }
 
-// struct Pipe {
-//     prev: Rc<Pipe>,
-//     next: Rc<Pipe>,
-// }
-
 struct Diagram {
-    pipes: Vec<Pipe>,
+    pipes: Vec<Vec<Pipe>>,
 }
 
 impl Diagram {
     fn from(input: &str) -> Self {
+        let pipes: Vec<Vec<Pipe>> = input
+            .lines()
+            .map(|line| line.chars().map(|c| Pipe::from(c)).collect())
+            .collect();
+
+        let mut grid: HashMap<(usize, usize), Vec<(usize, usize)>> = HashMap::new();
+        for i in 0..pipes.len() {
+            for j in 0..pipes[0].len() {
+                match pipes[i][j] {
+                    Pipe::Start => grid
+                        .insert((i, j), Vec::new())
+                        .expect("Failed to insert vector into grid!"),
+                    Pipe::Vertical => {
+                        grid.insert((i, j), Vec::new())
+                            .expect("Failed to insert vector into grid!");
+                        grid[&(i, j)].push((i, j + 1));
+                        grid[&(i, j)].push((i, j - 1));
+                    }
+                    Pipe::Horizontal => {
+                        grid.insert((i, j), Vec::new())
+                            .expect("Failed to insert vector into grid!");
+                        grid[&(i, j)].push((i + 1, j));
+                        grid[&(i, j)].push((i - 1, j));
+                    }
+                    Pipe::NorthEast => {
+                        grid.insert((i, j), Vec::new())
+                            .expect("Failed to insert vector into grid!");
+                        grid[&(i, j)].push((i, j + 1));
+                        grid[&(i, j)].push((i + 1, j));
+                    }
+                    Pipe::NorthWest => {
+                        grid.insert((i, j), Vec::new())
+                            .expect("Failed to insert vector into grid!");
+                        grid[&(i, j)].push((i, j + 1));
+                        grid[&(i, j)].push((i - 1, j));
+                    }
+                    Pipe::SouthEast => {
+                        grid.insert((i, j), Vec::new())
+                            .expect("Failed to insert vector into grid!");
+                        grid[&(i, j)].push((i, j - 1));
+                        grid[&(i, j)].push((i + 1, j));
+                    }
+                    Pipe::SouthWest => {
+                        grid.insert((i, j), Vec::new())
+                            .expect("Failed to insert vector into grid!");
+                        grid[&(i, j)].push((i, j - 1));
+                        grid[&(i, j)].push((i - 1, j));
+                    }
+                    Pipe::Nil => grid.insert((i, j), Vec::new()),
+                };
+            }
+        }
+
+        println!("Grid: {:?}", grid);
+
+        println!("Pipes: {:?}", pipes);
+
         Self { pipes: Vec::new() }
     }
 }
-
-// -------------------------------------------------------
-// Helper Functions
-// -------------------------------------------------------
 
 // -------------------------------------------------------
 // Main Program Logic
@@ -98,7 +160,7 @@ mod tests {
     }
 
     #[test]
-    fn test1_pt1() {
+    fn test1_pt2() {
         let puzzle_input = "\
 ..F7.
 .FJ|.
@@ -107,7 +169,7 @@ SJ.L7
 LJ...\
 ";
 
-        assert_eq!(pt1(puzzle_input), 4);
+        assert_eq!(pt1(puzzle_input), 8);
     }
 
     //     #[test]
