1fae24158b8ebb111c9977dd547ae6a4271ad4ac
Kodlak15 <stanlcod15@gmail.com>
2023-12-15 00:43:43 -0800

    Work on day 15

--- src/day15/day15.rs
+++ src/day15/day15.rs
@@ -9,11 +9,39 @@ use crate::read_input;
 // -------------------------------------------------------
 
 // -------------------------------------------------------
+// Helper Functions
+// -------------------------------------------------------
+
+fn hash(mut current: u32, c: char) -> u32 {
+    current += c as u32;
+    current *= 17;
+    current %= 256;
+
+    current
+}
+
+// -------------------------------------------------------
 // Main Program Logic
 // -------------------------------------------------------
 
-fn pt1(input: &str) -> usize {
-    0
+fn pt1(input: &str) -> u32 {
+    let strings: Vec<String> = input.split(",").map(|s| s.to_string()).collect();
+
+    strings
+        .iter()
+        .map(|s| {
+            let mut current = 0;
+
+            for c in s.chars() {
+                current = match c {
+                    '\n' => current,
+                    _ => hash(current, c),
+                }
+            }
+
+            current
+        })
+        .sum()
 }
 
 fn pt2(input: &str, cycles: usize) -> usize {
@@ -37,11 +65,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_pt1() {
-        let puzzle_input = "\
-";
+    fn test_hash() {
+        let mut current = 0;
 
-        assert_eq!(pt1(puzzle_input), 136);
+        for c in "HASH".chars() {
+            println!("{:?} -> {:?}", c, c as u32);
+            current = hash(current, c)
+        }
+
+        assert_eq!(current, 52);
+    }
+
+    #[test]
+    fn test_pt1() {
+        let puzzle_input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+
+        assert_eq!(pt1(puzzle_input), 1320);
     }
 
     //     #[test]
